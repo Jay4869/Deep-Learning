c824571e07151d824b2660ce192c78ca1ea7f903
Jay4869 <jay4869@gmail.com>
2019-11-28 00:36:10 -0500

    update attention block

--- Model/Attention_Block.py
+++ Model/Attention_Block.py
@@ -18,20 +18,53 @@ input >> Max Pooling >> Residual Block (p)
 
 def Attention_Block(input, in_channel, out_channel, encoder_depth=1):
 
+    """
+    :param input: The input of the Residual_Unit. Should be a 4D array like (batch_num, img_len, img_len, channel_num)
+    :param in_channel: The 4-th dimension (channel number) of input matrix. For example, in_channel=3 means the input contains 3 channels.
+    :param out_channel: The 4-th dimension (channel number) of output matrix. For example, out_channel=5 means the output contains 5 channels (feature maps).
+    """
+
     # initial Attention Module parameters
     p = 1
     t = 2
     r = 1
+    skip_connections = []
 
-    x = MaxPooling2D(input, 3, padding='valid', strides=2)
-
-    for i in range(r):
-        x = Residual_Unit(x)
-
-    # Soft Mask Branch
-    x = MaxPooling2D(input, 3, padding='valid', strides=2)
+    # pre-activation Residual Unit
+    for i in range(p):
+        x = Residual_Unit(input, in_channel // 4, out_channel)
 
     # Trunk Branch
     for i in range(t):
-        x = Residual_Unit(x)
+        Trunck_output = Residual_Unit(x)
+
+    # Soft Mask Branch
+    ## down sampling
+    x = MaxPooling2D(padding='same')(x)
+    for i in range(r):
+        x = Residual_Unit(x, in_channel // 4, out_channel)
+
+    ## skip connections
+    x = Residual_Unit(x, in_channel // 4, out_channel)
+    skip_connections.append(x)
+
+    ## down sampling
+    x = MaxPooling2D(padding='same')(x)
+    for i in range(r):
+        x = Residual_Unit(x, in_channel // 4, out_channel)
+
+
+    skip_connections = list(reversed(skip_connections))
+
+    ## up sampling
+    for i in range(r):
+        x = Residual_Unit(x, in_channel // 4, out_channel)
+    x = UpSampling2D()(x)
+    
+    ## skip connections
+    output_soft_mask = Add()([output_soft_mask, skip_connections[i]])
+
+
+
+
 
