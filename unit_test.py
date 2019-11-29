1178e6ee1751d9ae8dfafdbd8aae3c75ce6ff779
Jay4869 <jay4869@gmail.com>
2019-11-28 21:14:15 -0500

    add attention block tester

--- unit_test.py
+++ unit_test.py
@@ -2,13 +2,20 @@ import unittest
 import numpy as np
 import tensorflow as tf
 from Model import Residual_Unit
+from Model import Attention_Block
 
 class MyTestCase(unittest.TestCase):
+
+    image = tf.random.normal(shape=[1, 224, 224, 3])
+
     def test_Residual_Unit(self):
-        image = tf.random.normal(shape=[1, 224, 224, 3])
-        output = Residual_Unit(image, 64, 256)
+        output = Residual_Unit(self.image, 64, 256)
         self.assertEqual(output.shape, [1,224,224,256])
 
+
+    def test_Attention_Block(self):
+        output = Attention_Block(self.image, 256, 256)
+        self.assertEqual(output.shape, [1,224,224,256])
 
 if __name__ == '__main__':
     unittest.main()
