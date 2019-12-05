f9dc16508b63091703d28dddfe5ec48639b1d5e1
Jay4869 <jay4869@gmail.com>
2019-12-05 17:02:38 -0500

    update init

--- Model/__init__.py
+++ Model/__init__.py
@@ -1,4 +1,4 @@
 from .Residual_Unit import Residual_Unit
 from .Attention_Block import Attention_Block
-# from .AttentionResNet56 import AttentionResNet56
+from .AttentionResNet56 import AttentionResNet56
 from .AttentionResNet56_mini import AttentionResNet56_mini
