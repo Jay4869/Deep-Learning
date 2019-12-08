eb2597182639be6113d1c23c8104fa3b9e3a3020
Jay4869 <jay4869@gmail.com>
2019-12-08 17:07:48 -0500

    update

--- Model/Residual_Unit.py
+++ Model/Residual_Unit.py
@@ -27,7 +27,7 @@ def Residual_Unit(input, in_channel, out_channel, stride=1):
 
     # initialize as the input (identity) data
     shortcut = input
-    print(input.shape)
+    print(int(input.shape))
     # RestNet module
     x = BatchNormalization()(input)
     x = Activation('relu')(x)
