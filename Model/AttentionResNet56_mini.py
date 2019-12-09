aaab46fe42418c2529a10229e36358052791d632
Jay4869 <jay4869@gmail.com>
2019-12-08 23:14:24 -0500

    update

--- Model/AttentionResNet56_mini.py
+++ Model/AttentionResNet56_mini.py
@@ -39,7 +39,7 @@ def AttentionResNet56_mini(shape, in_channel, kernel_size, n_classes, dropout=No
     x = Attention_Block(x, skip=2)
 
     in_channel = out_channel // 2
-    out_channel = in_channel * 4
+    out_channel = in_channel * 2
     x = Residual_Unit(x, in_channel, out_channel, stride=2)  # 8x8x256
     x = Attention_Block(x, skip=1)
 
@@ -49,7 +49,7 @@ def AttentionResNet56_mini(shape, in_channel, kernel_size, n_classes, dropout=No
     # x = Attention_Block(x, skip=1)
 
     in_channel = out_channel // 2
-    out_channel = in_channel * 4
+    out_channel = in_channel * 2
     x = Residual_Unit(x, in_channel, out_channel, stride=1)  # 4x4x1024
     x = Residual_Unit(x, out_channel, out_channel)
     x = Residual_Unit(x, out_channel, out_channel)
@@ -59,12 +59,12 @@ def AttentionResNet56_mini(shape, in_channel, kernel_size, n_classes, dropout=No
     x = AveragePooling2D(pool_size=4, strides=1)(x)  # 1x1x1024
     x = Flatten()(x)
 
-    if dropout:
-        x = Dropout(dropout)(x)
-    x = Dense(out_channel, kernel_regularizer=l2(regularization), activation='relu')(x)
-    if dropout:
-        x = Dropout(dropout)(x)
-    x = Dense(out_channel, kernel_regularizer=l2(regularization), activation='relu')(x)
+    # if dropout:
+    #     x = Dropout(dropout)(x)
+    # x = Dense(out_channel, kernel_regularizer=l2(regularization), activation='relu')(x)
+    # if dropout:
+    #     x = Dropout(dropout)(x)
+    # x = Dense(out_channel, kernel_regularizer=l2(regularization), activation='relu')(x)
 
     output = Dense(n_classes, kernel_regularizer=l2(regularization), activation='softmax')(x)
     model = Model(input_data, output)
