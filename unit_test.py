0918f8cbee744a235a7489ebb9644b64fa5a1908
Jay4869 <jay4869@gmail.com>
2019-12-11 19:54:41 -0500

    update unit test

--- unit_test.py
+++ unit_test.py
@@ -3,7 +3,7 @@ import numpy as np
 import tensorflow as tf
 from Model import Residual_Unit
 from Model import Attention_Block
-from Model import AttentionResNet56
+from Model import Attention_Block_NAL
 
 class MyTestCase(unittest.TestCase):
 
@@ -18,13 +18,21 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(output.shape, [1,112,112,256])
 
     def test_Attention_Block(self):
-        image = tf.random.normal(shape=[1,14,14,256])
-        output = Attention_Block(image, skip=1)
-        self.assertEqual(output.shape, [1,14,14,256])
+        output = Attention_Block(self.image, skip=1)
+        self.assertEqual(output.shape, [1,224,224,3])
 
     def test_Attention_Block2(self):
         output = Residual_Unit(self.image, 64, 256, stride=2)
         output = Attention_Block(output, skip=2)
+        self.assertEqual(output.shape, [1,112,112,256])
+
+    def test_Attention_Block_NAL(self):
+        output = Attention_Block_NAL(self.image, skip=1)
+        self.assertEqual(output.shape, [1,224,224,3])
+
+    def test_Attention_Block_NAL2(self):
+        output = Residual_Unit(self.image, 64, 256, stride=2)
+        output = Attention_Block_NAL(output, skip=2)
         self.assertEqual(output.shape, [1,112,112,256])
 
 
