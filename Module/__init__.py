6558b6d4ba58c719228cbe45c0860dbd5e858fe6
Jay4869 <jay4869@gmail.com>
2019-12-28 15:27:54 -0500

    init

--- Module/__init__.py
+++ Module/__init__.py
@@ -1,4 +1,4 @@
 from .Residual_Unit import Residual_Unit
 from .Attention_Block import Attention_Block
 from .Attention_Block_NAL import Attention_Block_NAL
-from .Residual_Unit_new import Residual_Unit
+from .Residual_Unit_pre import Residual_Unit_pre
