542dc924035fe67f68ad4b8aa975a3cc3be8542e
Jay4869 <jay4869@gmail.com>
2019-12-28 15:30:11 -0500

    add Resdiual unit pre-activates

---
+++ Module/Residual_Unit_pre.py
@@ -0,0 +1,43 @@
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Add
+
+"""
+Residual Unit is implemented above proposed Residual Block
+input >> BN >> ReLU >> Conv2DLayer(3,3)
+      >> BN >> ReLU >> Conv2DLayer(3,3) + identify
+      >> output
+"""
+
+def Residual_Unit_pre(input, out_channel):
+
+    """
+    :param input: The input of the Residual_Unit. Should be a 4D array like (batch_num, img_len, img_len, channel_num)
+    :param out_channel: The 4-th dimension (channel number) of output matrix. For example, out_channel=5 means the output contains 5 channels (feature maps).
+    :param stride: Integer. The number of pixels to move between 2 neighboring receptive fields.
+    """
+
+    # initialize as the input (identity) data
+    shortcut = input
+    if shortcut.shape[-1] != out_channel:
+        shortcut = Conv2D(out_channel, (1, 1), padding='same')(shortcut)
+
+    # RestNet module
+    x = BatchNormalization()(input)
+    x = Activation('relu')(x)
+    x = Conv2D(out_channel, (3, 3), padding='same')(x)
+
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(out_channel, (3, 3), padding='same')(x)
+
+    x = Add()([x, shortcut])
+
+    return x
+
+
+
+
+
+
